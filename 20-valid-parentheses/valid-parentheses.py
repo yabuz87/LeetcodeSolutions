d088714c1f7ab61de487667ee393e78b5368cbf2
Yabets Solomon <144539218+yabuz87@users.noreply.github.com>
2024-06-28 11:32:13 +0300

    Time: 13 ms (72.20%) | Memory: 11.6 MB (77.14%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in mapping:
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
+        
