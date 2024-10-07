0f5fb34042a0fac3a602fcc5a3c374b38d562b1d
Yabets Solomon <144539218+yabuz87@users.noreply.github.com>
2024-10-07 15:23:05 +0300

    Time: 1 ms (99.46%) | Memory: 45.1 MB (44.86%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -5,6 +5,7 @@ class Solution {
     public List<String> fizzBuzz(int n) {
         List<String> result = new ArrayList<>();
         for (int i = 1; i <= n; i++) { // Start from 1
+        
             if (i % 3 == 0 && i % 5 == 0) {
                 result.add("FizzBuzz");
             } else if (i % 3 == 0) {
@@ -12,7 +13,8 @@ class Solution {
             } else if (i % 5 == 0) {
                 result.add("Buzz");
             } else {
-                result.add(String.valueOf(i)); // Corrected here
+                result.add(String.valueOf(i)); // Corrected here this part took almost 20 min till i figured out  how i can put this
+
             }
         }
         return result;
