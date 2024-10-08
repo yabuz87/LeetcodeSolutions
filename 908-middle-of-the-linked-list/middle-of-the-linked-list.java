b9fd3a55b1eaf4aa22ca4f4f2c681ec09d275895
Yabets Solomon <144539218+yabuz87@users.noreply.github.com>
2024-10-08 09:44:39 +0300

    Time: 0 ms (100.00%) | Memory: 41.5 MB (8.26%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -10,23 +10,36 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int counter = 0;
-        ListNode node = head;
+    //     int counter = 0;
+    //     ListNode node = head;
         
-        // First pass to count the number of nodes
-        while (head != null) {
-            head = head.next;
-            counter++;
-        }
+    //     // First pass to count the number of nodes
+    //     while (head != null) {
+    //         head = head.next;
+    //         counter++;
+    //     }
         
-        // Calculate the middle index
-        counter = counter / 2;
+    //     // Calculate the middle index
+    //     counter = counter / 2;
         
-        // Second pass to reach the middle node
-        for (int i = 0; i < counter; i++) {
-            node = node.next;
-        }
+    //     // Second pass to reach the middle node
+    //     for (int i = 0; i < counter; i++) {
+    //         node = node.next;
+    //     }
         
-        return node;
+    //     return node;
+
+
+    // }
+
+    ListNode middle=head;
+    ListNode end=head;
+    while(end!=null && end.next!=null)
+    {
+        middle=middle.next;
+        end=end.next.next;
+    }
+    return middle;
+    // the above code is written after i watched a video. the code does not improve the time or the space complexity but it is clearer code than the above one. gott?
     }
 }
