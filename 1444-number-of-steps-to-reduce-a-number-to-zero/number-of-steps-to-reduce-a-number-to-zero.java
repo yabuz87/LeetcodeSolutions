53256b4d4bacd744c0e9f6ef0639828a01334179
Yabets Solomon <144539218+yabuz87@users.noreply.github.com>
2024-10-08 09:21:55 +0300

    Time: 0 ms (100.00%) | Memory: 40.7 MB (19.60%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,26 +1,49 @@
 class Solution {
     public int numberOfSteps(int num) {
-        if(num==0)
-        return 0;
-        int counter=0;
-        while(num!=1)
-        {
-            if(num%2==0)
-            {
-                counter++;
-                num=num/2;
+    //     if(num==0)
+    //     return 0;
+    //     int counter=0;
+    //     while(num!=1)
+    //     {
+    //         if(num%2==0)
+    //         {
+    //             counter++;
+    //             num=num/2;
 
-            }
-            else
-            {
-                num=num-1;
-                counter++;
-            }
-        }
-        num=num-1;
-        counter++;
-        return counter;
+    //         }
+    //         else
+    //         {
+    //             num=num-1;
+    //             counter++;
+    //         }
+    //     }
+    //     num=num-1;
+    //     counter++;
+    //     return counter;
 
         
+    // }
+    // here also an insane approach with the concept of bitwise operation mainly
+    // the bitwise and operator and bitmask operation
+    int steps=0;
+    while(num>0)
+    {
+        if((num & 1)==0)
+        // before we used num%2==0;
+        {
+            num>>=1;
+            // before we used num/=2;
+        }
+        else
+        {
+            num--;
+
+        }
+        steps++;
     }
+    
+    return steps;
+
+
+}
 }
